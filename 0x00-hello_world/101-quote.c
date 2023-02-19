20cecab49712ae1868abda95b2f946034fadfbc8
Ibekwe Testimony <ibekwetestimony3@gmail.com>
2023-02-19 13:50:04 -0800

    Update 101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,12 +1,13 @@
 #include <stdio.h>
-#include <unistd.h>
+
 /**
- * main - A c program that prints a line to standard error
+ * main - Entry point
  *
- * Return: Always 1 (Success)
+ * Return: 1
  */
 int main(void)
 {
-	fputs("and that piece of art is useful\"- Dora Korpar, 2015-10-19\n", stderr);
+	fprintf(stderr, "%s", "and that piece of art is useful\"");
+	fprintf(stderr, "%s", " - Dora Korpar, 2015-10-19\n");
 	return (1);
 }
