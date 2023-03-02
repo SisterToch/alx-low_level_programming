bf37062eaf8cb785c7052857c7af90d37b1232fd
SisterToch <ibekwetestimony3@gmail.com>
2023-03-02 08:57:54 -0800

    strncpy

---
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ *_strncpy - function that copies a string
+ *
+ * @src: The source of strings
+ * @dest: The destination of the string
+ * @n: The length of int
+ *
+ * Return: pointer to the resulting string dest
+ */
+char *_strncpy(char *dest, char *src, int n)
+{
+	int i;
+
+	for (i = 0 ; i < n ; i++)
+		dest[i] = src[i];
+	return (dest);
+}
